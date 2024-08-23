053eb3679c7f48efc9b73afbdee7bc9391ab0077
VMarquesin <157024346+VMarquesin@users.noreply.github.com>
2024-08-23 20:36:06 -0300

    commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,33 @@
 import Image from "next/image";
+import {
+   MdLunchDining,
+   MdLocalBar,
+   MdDining,
+   MdIcecream,
+   MdFastfood,
+} from "react-icons/md";
+
 import styles from "./page.module.css";
+
+import Slider from "@/componentes/slider";
+import ProdutosHome from "@/componentes/produtos/produtosHome";
 
 export default function Home() {
    return (
       <div className="containerGlobal">
-         <h1 className={styles.tituloHomePage}>Aula Next JS</h1>
+         <Slider />
+
+         <div className={styles.tipos}>
+            <MdLunchDining className={styles.tpicon} />
+            <MdLocalBar className={styles.tpicon} />
+            <MdDining className={styles.tpicon} />
+            <MdIcecream className={styles.tpicon} />
+            <MdFastfood className={styles.tpicon} />
+         </div>
+
+         <ProdutosHome />
+
+         {/* <h1 className={styles.tituloHomePage}>Aula Next JS</h1>
          <h2>Exemplo h2</h2>
          <h3>Exemplo h3</h3>
          <p>Texto parágrafo</p>
@@ -24,8 +47,7 @@ export default function Home() {
                height={50}
                alt="Imagem"
                className={styles.imagemHomePage}
-            />
-         </div>
+            /> */}
       </div>
    );
 }
